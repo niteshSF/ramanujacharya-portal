003063ed5ee5915f283948a8d524f2bc82261ba2
Dillibabu2357 <dillibabudommaraju@gmail.com>
2025-04-10 14:37:26 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -15,3 +15,17 @@ export default defineConfig({
   //   port: 5173,
   // }
 })
+
+// import path from "path";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/ramanujacharya-portal-frontend/', // ✅ Correct repo name
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
